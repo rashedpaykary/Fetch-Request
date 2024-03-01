5f59e16b87f85d2d3ec668bc0f2fca379fd125a4
rashed <rashedpaykary@gmail.com>
2024-03-01 16:24:17 +0330

    repo

---
+++ main.js
@@ -0,0 +1,48 @@
+const container = document.querySelector(".container");
+
+function renderData(data) {
+  data.forEach((item) => {
+    const information = document.createElement("div");
+    information.className = "information";
+
+    const emailDiv = document.createElement("div");
+    emailDiv.textContent = item.email;
+    const userDiv = document.createElement("div");
+    userDiv.textContent = item.name;
+    const commentDiv = document.createElement("div");
+    commentDiv.textContent = item.body;
+
+    information.append(emailDiv, userDiv, commentDiv);
+    container.append(information);
+  });
+}
+
+// fetch("https://jsonplaceholder.typicode.com/comments")
+//   .then((res) => {
+//     //   console.log(res.json());
+//     //   json is promise
+//     // جیسان خشک و خالی خودش یه پرامیسه
+//     res.json().then((res) => {
+//       renderData(res);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log("error");
+//   });
+
+function fetchRequest(url, callback) {
+  fetch(url)
+    .then((res) => {
+      //   console.log(res.json());
+      //   json is promise
+      // جیسان خشک و خالی خودش یه پرامیسه
+      res.json().then((res) => {
+        callback(res);
+      });
+    })
+    .catch((error) => {
+      console.log("error");
+    });
+}
+
+fetchRequest("https://jsonplaceholder.typicode.com/comments", renderData);
